b857a6226cf042a7012ffb5c8e345f56cbe49d2f
Josh Lanesmith <josh.lanesmith@gmail.com>
2023-10-05 12:48:22 -0400

    first commit

---
+++ JLanesmithAssignment1/TicTacToeForm.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace JLanesmithAssignment1
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.picPlayerO = new System.Windows.Forms.PictureBox();
+			this.picPlayerX = new System.Windows.Forms.PictureBox();
+			this.picBottomRight = new System.Windows.Forms.PictureBox();
+			this.picBottomMiddle = new System.Windows.Forms.PictureBox();
+			this.picBottomLeft = new System.Windows.Forms.PictureBox();
+			this.picMiddleRight = new System.Windows.Forms.PictureBox();
+			this.picMiddleMiddle = new System.Windows.Forms.PictureBox();
+			this.picMiddleLeft = new System.Windows.Forms.PictureBox();
+			this.picTopRight = new System.Windows.Forms.PictureBox();
+			this.picTopMiddle = new System.Windows.Forms.PictureBox();
+			this.picTopLeft = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.picPlayerO)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPlayerX)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomRight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomMiddle)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomLeft)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleRight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleMiddle)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleLeft)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopRight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopMiddle)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopLeft)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Cooper Black", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Black;
+			this.label1.Location = new System.Drawing.Point(9, 7);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(495, 91);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "Tic Tac Toe";
+			// 
+			// picPlayerO
+			// 
+			this.picPlayerO.Image = global::JLanesmithAssignment1.Properties.Resources.player_o_icon;
+			this.picPlayerO.InitialImage = null;
+			this.picPlayerO.Location = new System.Drawing.Point(563, 179);
+			this.picPlayerO.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picPlayerO.Name = "picPlayerO";
+			this.picPlayerO.Size = new System.Drawing.Size(240, 260);
+			this.picPlayerO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picPlayerO.TabIndex = 11;
+			this.picPlayerO.TabStop = false;
+			// 
+			// picPlayerX
+			// 
+			this.picPlayerX.Image = global::JLanesmithAssignment1.Properties.Resources.player_x_icon;
+			this.picPlayerX.Location = new System.Drawing.Point(13, 179);
+			this.picPlayerX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picPlayerX.Name = "picPlayerX";
+			this.picPlayerX.Size = new System.Drawing.Size(240, 260);
+			this.picPlayerX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picPlayerX.TabIndex = 10;
+			this.picPlayerX.TabStop = false;
+			// 
+			// picBottomRight
+			// 
+			this.picBottomRight.BackColor = System.Drawing.Color.Ivory;
+			this.picBottomRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBottomRight.Location = new System.Drawing.Point(461, 358);
+			this.picBottomRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picBottomRight.Name = "picBottomRight";
+			this.picBottomRight.Size = new System.Drawing.Size(98, 106);
+			this.picBottomRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picBottomRight.TabIndex = 8;
+			this.picBottomRight.TabStop = false;
+			this.picBottomRight.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picBottomMiddle
+			// 
+			this.picBottomMiddle.BackColor = System.Drawing.Color.Ivory;
+			this.picBottomMiddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBottomMiddle.Location = new System.Drawing.Point(359, 358);
+			this.picBottomMiddle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picBottomMiddle.Name = "picBottomMiddle";
+			this.picBottomMiddle.Size = new System.Drawing.Size(98, 106);
+			this.picBottomMiddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picBottomMiddle.TabIndex = 7;
+			this.picBottomMiddle.TabStop = false;
+			this.picBottomMiddle.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picBottomLeft
+			// 
+			this.picBottomLeft.BackColor = System.Drawing.Color.Ivory;
+			this.picBottomLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picBottomLeft.Location = new System.Drawing.Point(257, 358);
+			this.picBottomLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picBottomLeft.Name = "picBottomLeft";
+			this.picBottomLeft.Size = new System.Drawing.Size(98, 106);
+			this.picBottomLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picBottomLeft.TabIndex = 6;
+			this.picBottomLeft.TabStop = false;
+			this.picBottomLeft.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picMiddleRight
+			// 
+			this.picMiddleRight.BackColor = System.Drawing.Color.Ivory;
+			this.picMiddleRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picMiddleRight.Location = new System.Drawing.Point(461, 247);
+			this.picMiddleRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picMiddleRight.Name = "picMiddleRight";
+			this.picMiddleRight.Size = new System.Drawing.Size(98, 106);
+			this.picMiddleRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picMiddleRight.TabIndex = 5;
+			this.picMiddleRight.TabStop = false;
+			this.picMiddleRight.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picMiddleMiddle
+			// 
+			this.picMiddleMiddle.BackColor = System.Drawing.Color.Ivory;
+			this.picMiddleMiddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picMiddleMiddle.Location = new System.Drawing.Point(359, 247);
+			this.picMiddleMiddle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picMiddleMiddle.Name = "picMiddleMiddle";
+			this.picMiddleMiddle.Size = new System.Drawing.Size(98, 106);
+			this.picMiddleMiddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picMiddleMiddle.TabIndex = 4;
+			this.picMiddleMiddle.TabStop = false;
+			this.picMiddleMiddle.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picMiddleLeft
+			// 
+			this.picMiddleLeft.BackColor = System.Drawing.Color.Ivory;
+			this.picMiddleLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picMiddleLeft.Location = new System.Drawing.Point(257, 247);
+			this.picMiddleLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picMiddleLeft.Name = "picMiddleLeft";
+			this.picMiddleLeft.Size = new System.Drawing.Size(98, 106);
+			this.picMiddleLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picMiddleLeft.TabIndex = 3;
+			this.picMiddleLeft.TabStop = false;
+			this.picMiddleLeft.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picTopRight
+			// 
+			this.picTopRight.BackColor = System.Drawing.Color.Ivory;
+			this.picTopRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picTopRight.Location = new System.Drawing.Point(461, 136);
+			this.picTopRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picTopRight.Name = "picTopRight";
+			this.picTopRight.Size = new System.Drawing.Size(98, 106);
+			this.picTopRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picTopRight.TabIndex = 2;
+			this.picTopRight.TabStop = false;
+			this.picTopRight.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picTopMiddle
+			// 
+			this.picTopMiddle.BackColor = System.Drawing.Color.Ivory;
+			this.picTopMiddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picTopMiddle.Location = new System.Drawing.Point(359, 136);
+			this.picTopMiddle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picTopMiddle.Name = "picTopMiddle";
+			this.picTopMiddle.Size = new System.Drawing.Size(98, 106);
+			this.picTopMiddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picTopMiddle.TabIndex = 1;
+			this.picTopMiddle.TabStop = false;
+			this.picTopMiddle.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// picTopLeft
+			// 
+			this.picTopLeft.BackColor = System.Drawing.Color.Ivory;
+			this.picTopLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picTopLeft.Location = new System.Drawing.Point(257, 136);
+			this.picTopLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.picTopLeft.Name = "picTopLeft";
+			this.picTopLeft.Size = new System.Drawing.Size(98, 106);
+			this.picTopLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picTopLeft.TabIndex = 0;
+			this.picTopLeft.TabStop = false;
+			this.picTopLeft.Click += new System.EventHandler(this.BoardSquare_Click);
+			// 
+			// TicTacToeForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.ClientSize = new System.Drawing.Size(815, 583);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.picPlayerO);
+			this.Controls.Add(this.picPlayerX);
+			this.Controls.Add(this.picBottomRight);
+			this.Controls.Add(this.picBottomMiddle);
+			this.Controls.Add(this.picBottomLeft);
+			this.Controls.Add(this.picMiddleRight);
+			this.Controls.Add(this.picMiddleMiddle);
+			this.Controls.Add(this.picMiddleLeft);
+			this.Controls.Add(this.picTopRight);
+			this.Controls.Add(this.picTopMiddle);
+			this.Controls.Add(this.picTopLeft);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "TicTacToeForm";
+			this.Text = "Tic Tac Toe";
+			this.Load += new System.EventHandler(this.TicTacToeForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.picPlayerO)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPlayerX)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomRight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomMiddle)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBottomLeft)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleRight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleMiddle)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMiddleLeft)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopRight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopMiddle)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picTopLeft)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picTopLeft;
+        private System.Windows.Forms.PictureBox picTopMiddle;
+        private System.Windows.Forms.PictureBox picTopRight;
+        private System.Windows.Forms.PictureBox picMiddleLeft;
+        private System.Windows.Forms.PictureBox picMiddleMiddle;
+        private System.Windows.Forms.PictureBox picMiddleRight;
+        private System.Windows.Forms.PictureBox picBottomLeft;
+        private System.Windows.Forms.PictureBox picBottomMiddle;
+        private System.Windows.Forms.PictureBox picBottomRight;
+		private System.Windows.Forms.PictureBox picPlayerX;
+		private System.Windows.Forms.PictureBox picPlayerO;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ JLanesmithAssignment1/TicTacToeForm.cs
@@ -0,0 +1,349 @@
+﻿/* 
+ * TicTacToeForm.cs
+ * Assignment 1
+ * Revision History
+ *      Josh Lanesmith 2023-09-14: Created
+ *      Josh Lanesmith 2023-09-20: Add player icon images with speech bubbles
+ *      Josh Lanesmith 2023-09-21: Add starting sequence with player icons
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JLanesmithAssignment1
+{
+	/// <summary>
+	/// A class for a simple game of Tic Tac Toe.
+	/// </summary>
+	public partial class TicTacToeForm : Form
+	{
+		//Define enum used to track the board square assignments and whose turn it is
+		enum Player
+		{
+			empty, x, o
+		}
+
+		// Declaring class variables
+		Player turn;
+		Player[,] board = new Player[3, 3];
+		int turnCounter = 1;
+		const int TOGGLE_TURN_ANIMATION_DELAY = 200;
+
+		// Delclaring image paths
+		static readonly Image playerIconX = Properties.Resources.player_x_icon;
+		static readonly Image playerIconReadyX = Properties.Resources.player_x_are_you_ready;
+		static readonly Image playerIconTurnX = Properties.Resources.player_x_my_turn;
+		static readonly Image playerIconWinX = Properties.Resources.player_x_i_win;
+		static readonly Image boardIconX = Properties.Resources.x_image;
+
+		static readonly Image playerIconO = Properties.Resources.player_o_icon;
+		static readonly Image playerIconPlayO = Properties.Resources.player_o_lets_play;
+		static readonly Image playerIconTurnO = Properties.Resources.player_o_my_turn;
+		static readonly Image playerIconWinO = Properties.Resources.player_o_i_win;
+		static readonly Image boardIconO = Properties.Resources.o_image;
+
+		// Declare dictionary to store the board coordinates of each picture box
+		// Dictionary initialized in form constructor
+		Dictionary<PictureBox, int[]> pictureBoxCoordinates;
+
+		// Define starting sequence for when the game first loads
+		#region Starting Sequence
+
+		const int STARTING_SEQUENCE_STEPS_LENGTH = 3;
+		const int STARTING_SEQUENCE_INTERVAL = 1500;
+		int startingSequenceStepCounter = 0;
+
+		readonly Image[] playerStartingImageSequenceX = new Image[STARTING_SEQUENCE_STEPS_LENGTH]{
+			playerIconReadyX,
+			playerIconX,
+			playerIconTurnX
+		};
+
+		readonly Image[] playerStartingImageSequenceO = new Image[STARTING_SEQUENCE_STEPS_LENGTH]{
+			playerIconO,
+			playerIconPlayO,
+			playerIconO
+		};
+
+		System.Windows.Forms.Timer startingSequenceTimer = new System.Windows.Forms.Timer();
+
+		/// <summary>
+		/// Event handler to run on a timer and display the sequence of starting images.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void RunStartingSequence(object sender, EventArgs e)
+		{
+			// Display the player icons in the predefined sequence order
+			picPlayerX.Image = playerStartingImageSequenceX[startingSequenceStepCounter];
+			picPlayerO.Image = playerStartingImageSequenceO[startingSequenceStepCounter];
+
+			// Increment the starting sequence step counter
+			startingSequenceStepCounter++;
+
+			// Stop the timer from ticking when the step counter reaches the starting sequence length
+			if (startingSequenceStepCounter >= STARTING_SEQUENCE_STEPS_LENGTH)
+			{
+				startingSequenceTimer.Stop();
+
+				// Re-enable the Form UI after the startingSequenceTimer has been stopped 
+				this.Enabled = true;
+			}
+		}
+
+		#endregion
+
+		#region Class Methods
+
+		/// <summary>
+		/// Assign the selected board square to the current player if the square is empty.
+		/// </summary>
+		/// <param name="square">PictureBox control associated with the board.</param>
+		/// <returns>return -1 if the square is already occupied, return 1 if the square was assigned successfully</returns>
+		private int AssignSquare(PictureBox square)
+		{
+			int row = pictureBoxCoordinates[square][0];
+			int col = pictureBoxCoordinates[square][1];
+
+			// Check if the selected board location is already occupied
+			if (board[row, col] != Player.empty)
+			{
+				return -1;
+			}
+
+			// Set the square image and board location based on whose turn it is
+			if (turn == Player.x)
+			{
+				square.Image = boardIconX;
+			}
+			else
+			{
+				square.Image = boardIconO;
+			}
+			board[row, col] = turn;
+
+			return 1;
+		}
+
+		/// <summary>
+		/// Check if the current player won after taking their turn or if the all squares are full and it ends in a tie.
+		/// </summary>
+		/// <returns>return -1 if game continues, return 0 if game ends in a tie, return 1 if current player wins.</returns>
+		private int CheckWin()
+		{
+			// Return -1 to continue game if less than 5 turns have been taken
+			if (turnCounter < 5)
+			{
+				return -1;
+			}
+
+			// Return 1 to indicate a win if any of the win conditions return true
+			if (board[0, 0] == turn)
+			{
+				if (board[0, 1] == turn && board[0, 2] == turn)
+				{
+					return 1;
+				}
+				else if (board[1, 1] == turn && board[2, 2] == turn)
+				{
+					return 1;
+				}
+				else if (board[1, 0] == turn && board[2, 0] == turn)
+				{
+					return 1;
+				}
+			}
+			if (board[1, 1] == turn)
+			{
+				if (board[1, 0] == turn && board[1, 2] == turn)
+				{
+					return 1;
+				}
+				else if (board[0, 2] == turn && board[2, 0] == turn)
+				{
+					return 1;
+				}
+				else if (board[0, 1] == turn && board[2, 1] == turn)
+				{
+					return 1;
+				}
+			}
+			if (board[2, 2] == turn)
+			{
+				if (board[2, 0] == turn && board[2, 1] == turn)
+				{
+					return 1;
+				}
+				else if (board[0, 2] == turn && board[1, 2] == turn)
+				{
+					return 1;
+				}
+			}
+
+			// Return 0 to indicate a tie if 9 turns have been completed without a win
+			if (turnCounter == 9)
+			{
+				return 0;
+			}
+
+			return -1;
+		}
+
+		/// <summary>
+		/// Increases the turn counter and updates who's turn it is 'x' or 'o'.
+		/// </summary>
+		private void ToggleTurn()
+		{
+			if (turn == Player.x)
+			{
+				turn = Player.o;
+				picPlayerX.Image = playerIconX;
+				Thread.Sleep(TOGGLE_TURN_ANIMATION_DELAY);
+				picPlayerO.Image = playerIconTurnO;
+			}
+			else
+			{
+				turn = Player.x;
+				picPlayerO.Image = playerIconO;
+				Thread.Sleep(TOGGLE_TURN_ANIMATION_DELAY);
+				picPlayerX.Image = playerIconTurnX;
+			}
+
+			turnCounter++;
+		}
+
+
+		/// <summary>
+		/// Reset the board for a new game. Set the turn to Player.x, reset the turn counter to 1, 
+		/// and reset all board squares to empty.
+		/// </summary>
+		private void ResetBoard()
+		{
+			// Set first turn to always be player 'X'
+			turn = Player.x;
+			turnCounter = 1;
+
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					board[i, j] = Player.empty;
+				}
+			}
+
+			foreach (PictureBox square in this.Controls.OfType<PictureBox>())
+			{
+				square.Image = null;
+			}
+
+			// Set player icon images to indicate player X's turn
+			picPlayerX.Image = playerIconTurnX;
+			picPlayerO.Image = playerIconO;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Constructor of the Form.
+		/// </summary>
+		public TicTacToeForm()
+		{
+			InitializeComponent();
+
+			// Define coordinates of each picture box square of the board
+			pictureBoxCoordinates = new Dictionary<PictureBox, int[]>
+			{
+				{ picTopLeft, new int[]{ 0, 0 } },
+				{ picTopMiddle, new int[]{ 0, 1 } },
+				{ picTopRight, new int[]{ 0, 2 } },
+				{ picMiddleLeft, new int[]{ 1, 0 } },
+				{ picMiddleMiddle, new int[]{ 1, 1 } },
+				{ picMiddleRight, new int[]{ 1, 2 } },
+				{ picBottomLeft, new int[]{ 2, 0 } },
+				{ picBottomMiddle, new int[]{ 2, 1 } },
+				{ picBottomRight, new int[]{ 2, 2 } },
+			};
+
+		}
+
+		/// <summary>
+		/// This method executes when the Form loads and sets the first player as 'x'.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void TicTacToeForm_Load(object sender, EventArgs e)
+		{
+			// Disable the Form UI to block user input while the form loads and the startingSequenceTimer runs
+			this.Enabled = false;
+
+			turn = Player.x;
+
+			picPlayerX.Image = playerIconX;
+			picPlayerO.Image = playerIconO;
+
+			startingSequenceTimer.Interval = STARTING_SEQUENCE_INTERVAL;
+			startingSequenceTimer.Tick += new EventHandler(RunStartingSequence);
+			startingSequenceTimer.Start();
+		}
+
+		/// <summary>
+		/// This method executes when any of the board squares are clicked and assigns the square to the current player.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void BoardSquare_Click(object sender, EventArgs e)
+		{
+			int turnCompleted = AssignSquare((PictureBox)sender);
+
+			// If AssignSquare returns -1 to turn completed then return out of current event without toggling turn
+			if (turnCompleted == -1)
+			{
+				return;
+			}
+
+			int turnStatus = CheckWin();
+
+			// Evaluate the result of the CheckWin() method and display winner or tie message if applicable.
+			if (turnStatus == 1)
+			{
+				switch (turn)
+				{
+					case Player.x:
+						Thread.Sleep(TOGGLE_TURN_ANIMATION_DELAY);
+						picPlayerX.Image = playerIconWinX;
+						picPlayerO.Image = playerIconO;
+						MessageBox.Show("X wins!", "Game End", MessageBoxButtons.OK);
+						break;
+					case Player.o:
+						Thread.Sleep(TOGGLE_TURN_ANIMATION_DELAY);
+						picPlayerX.Image = playerIconX;
+						picPlayerO.Image = playerIconWinO;
+						MessageBox.Show("O wins!", "Game End", MessageBoxButtons.OK);
+						break;
+					default:
+						break;
+				}
+				ResetBoard();
+			}
+			else if (turnStatus == 0)
+			{
+				picPlayerX.Image = playerIconX;
+				picPlayerO.Image = playerIconO;
+				MessageBox.Show("Tie", "Game End", MessageBoxButtons.OK);
+				ResetBoard();
+			}
+			else
+			{
+				ToggleTurn();
+			}
+		}
+	}
+}
